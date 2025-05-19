c3cf6379e7ea5796dbed08027eb3bfc482577f2d
duong.ph2 <duong.ph2@shbfinance.com.vn>
2025-05-19 17:30:01 +0700

    [Home]: - Changing color hover

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -12,19 +12,19 @@ export default function HomePage() {
             <div className={'w-[1200px] pt-12 pb-16'}>
                 <h1 className={'mb-8 text-2xl font-medium'}>Browse By Category</h1>
                 <div className={'flex justify-around items-center'}>
-                    <div className={'flex flex-col gap-2 items-center justify-center w-[200px] h-[150px] bg-[#ededed] rounded-2xl cursor-pointer transition duration-200 ease-in-out hover:bg-[#b7b6b6] hover:scale-110'}>
+                    <div className={'flex flex-col gap-2 items-center justify-center w-[200px] h-[150px] bg-[#ededed] rounded-2xl cursor-pointer transition duration-200 ease-in-out hover:bg-[#d1d0d0] hover:scale-110'}>
                         <LuSmartphone className={'text-5xl'} />
                         <p className={'font-medium'}>{t('phones')}</p>
                     </div>
-                    <div className={'flex flex-col gap-2 items-center justify-center w-[200px] h-[150px] bg-[#ededed] rounded-2xl cursor-pointer transition duration-200 ease-in-out hover:bg-[#b7b6b6] hover:scale-110'}>
+                    <div className={'flex flex-col gap-2 items-center justify-center w-[200px] h-[150px] bg-[#ededed] rounded-2xl cursor-pointer transition duration-200 ease-in-out hover:bg-[#d1d0d0] hover:scale-110'}>
                         <TbDeviceWatchStats className={'text-5xl'} />
                         <p>{t('smartsWatches')}</p>
                     </div>
-                    <div className={'flex flex-col gap-2 items-center justify-center w-[200px] h-[150px] bg-[#ededed] rounded-2xl cursor-pointer transition duration-200 ease-in-out hover:bg-[#b7b6b6] hover:scale-110'}>
+                    <div className={'flex flex-col gap-2 items-center justify-center w-[200px] h-[150px] bg-[#ededed] rounded-2xl cursor-pointer transition duration-200 ease-in-out hover:bg-[#d1d0d0] hover:scale-110'}>
                         <MdOutlineCameraAlt className={'text-5xl'} />
                         <p>{t('cameras')}</p>
                     </div>
-                    <div className={'flex flex-col gap-2 items-center justify-center w-[200px] h-[150px] bg-[#ededed] rounded-2xl cursor-pointer transition duration-200 ease-in-out hover:bg-[#b7b6b6] hover:scale-110'}>
+                    <div className={'flex flex-col gap-2 items-center justify-center w-[200px] h-[150px] bg-[#ededed] rounded-2xl cursor-pointer transition duration-200 ease-in-out hover:bg-[#d1d0d0] hover:scale-110'}>
                         <FaHeadphonesAlt className={'text-5xl'} />
                         <p>{t('headPhone')}</p>
                     </div>
