4a0fb9c842500963e17ef7ead2f77f2fd3b4eadf
DuongpH <eganpham.99@gmail.com>
2025-05-25 13:00:41 +0700

    multi language

--- src/components/product/product-info.tsx
+++ src/components/product/product-info.tsx
@@ -6,8 +6,13 @@ import {usePathname} from "@/i18n/navigation";
 import {useQuery} from "@tanstack/react-query";
 import {fetchProductById} from "@/utils/api/product";
 import {useTranslations} from "next-intl";
-import {GiPlainCircle} from "react-icons/gi";
+import {GiBattery75, GiCpu, GiPlainCircle} from "react-icons/gi";
 import { IColor } from "@/types/model/productDetails";
+import SpecificationItem from "@/components/product/SpecificationItem";
+import {SlScreenSmartphone} from "react-icons/sl";
+import {LuCpu} from "react-icons/lu";
+import {IoIosCamera} from "react-icons/io";
+import {IoCameraReverseSharp} from "react-icons/io5";
 
 const ProductInfo: FC<{ productID: string }> = ({ productID }) => {
     const [displayImages, setDisplayImages] = useState<string>('');
@@ -63,7 +68,8 @@ const ProductInfo: FC<{ productID: string }> = ({ productID }) => {
                                 </button>
                             )) }
                         </div>
-                        <Image src={displayImages} alt={data.name} width={0} height={0} className={'flex-1 h-auto'} unoptimized priority />
+                        { displayImages && <Image src={displayImages} alt={data.name} width={0} height={0} className={'flex-1 h-auto'} unoptimized priority /> }
+
                     </div>
                     <div className={'self-start'}>
                         <div className={'flex flex-col gap-6'}>
@@ -74,7 +80,10 @@ const ProductInfo: FC<{ productID: string }> = ({ productID }) => {
                                 <div className={'flex'}>
                                     { data.color!.map(ele => <GiPlainCircle
                                         key={ele.colorCode}
-                                        className={`bg-[${ele.colorCode}] cursor-pointer text-4xl`}
+                                        style={{
+                                            color: ele.colorCode,
+                                        }}
+                                        className={`cursor-pointer text-4xl`}
                                         onClick={() => handleSelectColorProduct(ele.colorCode)}
                                     />) }
                                 </div>
@@ -83,12 +92,40 @@ const ProductInfo: FC<{ productID: string }> = ({ productID }) => {
                                 { currentSelectedColorProduct?.details.map(detail => (<button
                                     key={detail.storage}
                                     disabled={detail.quantity === 0}
-                                    className={`border-1 rounded-[8px] px-7  py-2 cursor-pointer font-medium ${currentSelectedStorage && currentSelectedStorage === detail.storage ? '' : 'border-gray-500 text-gray-500'}`}
+                                    className={`border-1 rounded-[8px] px-7  py-2 cursor-pointer font-medium transition duration-200 ease-in-out ${currentSelectedStorage && currentSelectedStorage === detail.storage ? 'bg-gray-300 border-gray-300 text-gray-600 ' : 'border-gray-500 text-gray-500'}`}
                                     onClick={() => setCurrentSelectedStorage(detail.storage)}
                                 >
                                     {detail.storage}GB
                                 </button>)) }
                             </div>
+                        </div>
+                        <div className={'mt-8 w-[700px] flex flex-wrap gap-3'}>
+                            <SpecificationItem title={'Screen size'} info={data.specification.screenSize.toString()}>
+                                <SlScreenSmartphone className={'text-xl'} />
+                            </SpecificationItem>
+                            <SpecificationItem title={'CPU'} info={data.specification.cpu.toString()}>
+                                <GiCpu className={'text-xl'} />
+                            </SpecificationItem>
+                            <SpecificationItem title={'Number of Cores'} info={data.specification.coreCpu.toString()}>
+                                <LuCpu className={'text-xl'} />
+                            </SpecificationItem>
+                            <SpecificationItem title={'Main camera'} info={data.specification.mainCamera}>
+                                <IoIosCamera className={'text-xl'} />
+                            </SpecificationItem>
+                            <SpecificationItem title={'Front camera'} info={data.specification.frontCamera.toString()}>
+                                <IoCameraReverseSharp className={'text-xl'} />
+                            </SpecificationItem>
+                            <SpecificationItem title={'Battery capacity'} info={data.specification.batteryCapacity.toString()}>
+                                <GiBattery75 className={'text-xl'} />
+                            </SpecificationItem>
+                        </div>
+                        <div className={'flex mt-8 w-[566px] gap-4'}>
+                            <button className={'flex-1 rounded-[8px] border-1'}>
+                                Add to Wishlist
+                            </button>
+                            <button className={'flex-1 rounded-[8px] py-4 bg-[#000] text-white'}>
+                                Add to Cart
+                            </button>
                         </div>
                     </div>
                 </div>
